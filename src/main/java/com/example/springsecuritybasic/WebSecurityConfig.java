e91e99a4bec89ac676e924f170ffe4402bc341d1
hgs-study <hgstudy_@naver.com>
2021-03-10 22:08:37 +0900

    [ADD] Spring Security Setting

--- src/main/java/com/example/springsecuritybasic/WebSecurityConfig.java
+++ src/main/java/com/example/springsecuritybasic/WebSecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private CustomAuthProvider customAuthProvider;
+
     //사용자 요청 중 /resources/로 시작하는 요청은 제외
     @Override
     public void configure(WebSecurity web) throws Exception{
@@ -68,17 +69,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http
-//                .authorizeRequests() //요청을 어떻게 보안을 걸 것이냐 설정하는 것 (가장 중요)
+//            .authorizeRequests() //요청을 어떻게 보안을 걸 것이냐 설정하는 것 (가장 중요)
 //                .antMatchers("/","/home").permitAll() //css, js, img  파일 더 있을 것
 //                .anyRequest().authenticated()
 //                .and()
-//                .formLogin()
+//            .formLogin()
 //                .loginPage("/login")
 //                .permitAll() //모두 들어올 수 있게
 //                .and()
-//                .csrf()
+//            .csrf()
 //                .disable()
-//                .logout()
+//            .logout()
 //                .logoutSuccessUrl("/") //로그아웃 성공 후 url
 //                .permitAll();
 //    }
